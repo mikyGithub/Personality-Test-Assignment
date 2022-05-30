c45b7ac36ff45784b5547081ee72515f6656f063
ckassahun <54312715+mikyGithub@users.noreply.github.com>
2022-05-30 09:41:40 +0300

    Responsiveness Fixed

--- src/app/components/choice/choice.component.ts
+++ src/app/components/choice/choice.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Choice, Question } from './../../model/question';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-choice',
   templateUrl: './choice.component.html',
-  styleUrls: ['./choice.component.scss']
+  styleUrls: ['./choice.component.scss'],
 })
 export class ChoiceComponent implements OnInit {
+  @Input() choice: Choice = { option: '', personalityType: '' };
+  @Input() currentAnswer: Choice = { option: '', personalityType: '' };
 
-  constructor() { }
+  @Output() selectAnswerEmitter = new EventEmitter<Choice>();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onAnswerSelect(selectedAnswer: Choice):void{
+    this.selectAnswerEmitter.emit(selectedAnswer)
   }
-
 }
